f21f5b8aa7fa1f9eeacb29a687a603e70dbe8520
Menusoids <felipecm13@insper.al.edu.com.br>
2026-09-10 10:39:39 -0300

    Feature: deletar e editar nota

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>/delete',views.delete, name='delete'),
+    path('<int:id>/update', views.update, name='update'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -11,3 +11,21 @@ def index(request):
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request, id):
+    Note.objects.get(id=id).delete()
+    return redirect('index')
+
+def update(request, id):
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        note = Note.objects.get(id=id)
+        note.title = title
+        note.content = content
+        note.save()
+        return redirect('index')
+    else:
+        note = Note.objects.get(id=id)
+        return render(request, 'notes/update.html', {'note': note})
+
